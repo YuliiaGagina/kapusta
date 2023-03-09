45daa0df6b6ef151ea76722504d1ff54f38d2c90
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-03-09 09:18:01 +0200

    Оптимізація коду

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux'; // useDispatch, 
+import { useSelector } from 'react-redux'; // useDispatch,
 // import { getTransactionIncome, getTransactionExpense } from 'redux/Transactions/TransactionsOperations';
 import { selectMonthsStats } from 'redux/Transactions/selectors'; // , getIsloading
 import css from './Summary.module.css';
-
-
 
 export const Summary = () => {
   // const summaryMonth = 6; // Кількість місяців яку необхідно рендерити
@@ -12,50 +10,62 @@ export const Summary = () => {
   const stateMonts = useSelector(selectMonthsStats);
 
   const [listMonths, setlistMonths] = useState([]); // масив результатів
-  
 
   useEffect(() => {
-    const allMonths = ["Декабрь", "Ноябрь", "Октябрь", "Сентябрь", "Август", "Июль", "Июнь", "Май", "Апрель", "Март", "Февраль", "Январь"];
-    const allMonthsEn = {"Январь": "January", "Февраль": "February", "Март": "March", "Апрель": "April", "Май": "May",
-      "Июнь": "June",
-      "Июль": "July",
-      "Август": "August",
-      "Сентябрь": "September",
-      "Октябрь": "October",
-      "Ноябрь": "November",
-      "Декабрь": "December"
+    const listKeyMonths = [
+      'Декабрь',
+      'Ноябрь',
+      'Октябрь',
+      'Сентябрь',
+      'Август',
+      'Июль',
+      'Июнь',
+      'Май',
+      'Апрель',
+      'Март',
+      'Февраль',
+      'Январь',
+    ];
+    const listMonthsEng = {
+      Январь: 'January',
+      Февраль: 'February',
+      Март: 'March',
+      Апрель: 'April',
+      Май: 'May',
+      Июнь: 'June',
+      Июль: 'July',
+      Август: 'August',
+      Сентябрь: 'September',
+      Октябрь: 'October',
+      Ноябрь: 'November',
+      Декабрь: 'December',
     };
-  
-    let result = [];
-    allMonths.map((e)=>{
-      const s = stateMonts[e];
-      if( s === 'N/A') {return};
-      const m = allMonthsEn[e]
-      result = [...result, {month: m, value: s }]
-    });
 
-    setlistMonths(result)
+    const result = listKeyMonths
+      .map(e => {
+        return { month: listMonthsEng[e], value: stateMonts[e] };
+      })
+      .filter(e => e.value !== 'N/A');
+
+    setlistMonths(result);
   }, [stateMonts]);
 
   return (
     <div className={css.summaryContainer}>
       <h3 className={css.summaryTitle}>SUMMARY</h3>
       <ul className={css.summaryList}>
-        
         {listMonths.map(({ month, value }, edx) => (
           <li key={edx} className={css.summaryItem}>
             <p className={css.summaryDescription}>{month}</p>
             <p className={css.summaryDescription}> {value} </p>
           </li>
         ))}
-
       </ul>
     </div>
   );
 };
 
 export default Summary;
-
 
 // import { Summary } from 'components/Summary/Summary';
 // <Summary />
